4f4d7b7fbd543f76c9627b63980689cbd67f3207
JenZhang <52390463+JenUp@users.noreply.github.com>
2022-06-24 15:05:52 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Jen Zhang',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  //description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
   twitter: '',
